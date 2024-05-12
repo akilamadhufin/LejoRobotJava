a21b9db937ab97ff547d1c28f97d1a6b547b221f
GimhaniKaushalya <126944401+GimhaniKaushalya@users.noreply.github.com>
2024-05-12 10:57:22 +0300

    Update RunClass.java

--- Robot2web/src/app/RunClass.java
+++ Robot2web/src/app/RunClass.java
@@ -1,79 +1,3 @@
-////
-////package app;
-////
-////import lejos.hardware.Button;
-////import lejos.hardware.Sound;
-////import lejos.hardware.lcd.LCD;
-////import lejos.hardware.motor.Motor;
-////import lejos.utility.Delay;
-////import app.LineFollower;
-////
-////
-////public class RunClass {
-////    private static DataExchange DE;
-////    private static LineFollower LFObj;
-////    private static ObstacleDetector ODObj;
-////    private static SoundWave soundwave;
-////
-////    /**
-////     * 
-////     * This executes the main method
-////     */
-////
-////    public static void main(String[] args) {
-////        DE = new DataExchange();
-////        soundwave = new SoundWave();
-////        ODObj = new ObstacleDetector(DE, soundwave);
-////
-////        LFObj = new LineFollower();
-////
-////        Thread lineFollowerThread = new Thread(LFObj);
-////        Thread obstacleDetectorThread = new Thread(ODObj);
-////
-////        // Wait for the Enter key press to start
-////       
-////        while (!Button.ENTER.isDown()) {
-////           // Sound.beep();
-////           // Delay.msDelay(500);
-////        	 LCD.drawString("Press Enter to start", 0, 1);
-////            
-////        }
-////        LCD.clear();
-////
-////        // Start the threads
-////        lineFollowerThread.start();
-////        obstacleDetectorThread.start();
-////
-////        // Record the start time
-////        long startTime = System.currentTimeMillis();
-////
-////        // Wait for Escape key press to exit
-////        LCD.drawString("Press Escape to exit", 0, 1);
-////        while (!Button.ESCAPE.isDown()) {}
-////
-////        // Stop the motors
-////        Motor.C.stop(true);
-////        Motor.B.stop(true);
-////        
-////        // Calculate final elapsed time
-////        long currentTime = System.currentTimeMillis();
-////        long elapsedTime = (currentTime - startTime) / 1000;
-////
-////        // Display final elapsed time
-////        LCD.clear();
-////        LCD.drawString("Final Elapsed Time:", 0, 1);
-////        LCD.drawString(elapsedTime + "s", 0, 2);
-////        
-////        Delay.msDelay(5000);
-////
-////        // Clear the LCD and exit
-////        LCD.clear();
-////        LCD.drawString("Program finished", 0, 1);
-////        Delay.msDelay(500);
-////        System.exit(0);
-////    }
-////}
-//
 package app;
 
 import lejos.hardware.Button;
@@ -101,14 +25,14 @@ public class RunClass {
         Thread sendRobotDataThread = new Thread(robotdata);
         
         
-        // Wait for the Enter key press to start
+//      Wait for the Enter key press to start
         LCD.drawString("Press Enter to start", 0, 1);
         while (!Button.ENTER.isDown()) {}
 
-        // Clear the LCD before starting
+//      Clear the LCD before starting
         LCD.clear();
 
-        // Start the line follower thread
+//      Start the line follower thread
         lineFollowerThread.start();
         obstacleDetectorThread.start();
         
@@ -116,72 +40,20 @@ public class RunClass {
         robotdata.setLineFollower(LFObj);
         sendRobotDataThread.start();
         
-
-        // Notify the user that the robot is now following the line
-     //   LCD.drawString("Robot is following", 0, 1);
-      //  LCD.drawString("the line.", 0, 2);
-
-        // Wait for Escape key press to exit
-  //      LCD.drawString("Press Escape to exit", 0, 4);
+//      Wait for Escape key press to exit
+//      LCD.drawString("Press Escape to exit", 0, 4);
         while (!Button.ESCAPE.isDown()) {}
 
-        // Stop the motors
+//      Stop the motors
         Motor.C.stop(true);
         Motor.B.stop(true);
 
-        // Clear the LCD and exit
+//      Clear the LCD and exit
         LCD.clear();
-   //     LCD.drawString("Program finished", 0, 1);
+//      LCD.drawString("Program finished", 0, 1);
         Delay.msDelay(500);
         System.exit(0);
     }
 }
 
-
-//package app;
-//
-//import lejos.hardware.Button;
-//import lejos.hardware.lcd.LCD;
-//import lejos.hardware.motor.Motor;
-//import lejos.utility.Delay;
-//
-//public class RunClass {
-//    private static SendRobotData sendRobotData;
-//
-//    public static void main(String[] args) {
-//        sendRobotData = new SendRobotData();
-//        
-//        Thread sendRobotDataThread = new Thread(sendRobotData);
-//
-//        // Wait for the Enter key press to start
-//        LCD.drawString("Press Enter to start", 0, 1);
-//        while (!Button.ENTER.isDown()) {}
-//
-//        // Clear the LCD before starting
-//        LCD.clear();
-//
-//        // Start the sendRobotData thread
-//        sendRobotDataThread.start();
-//
-//        // Wait for Escape key press to exit
-//        LCD.drawString("Press Escape to exit", 0, 1);
-//        while (!Button.ESCAPE.isDown()) {}
-//
-//        // Stop sending data
-//        sendRobotData.stopSending();
-//
-//        // Wait for the sendRobotData thread to finish
-//        try {
-//            sendRobotDataThread.join();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        // Clear the LCD and exit
-//        LCD.clear();
-//        LCD.drawString("Program finished", 0, 1);
-//        Delay.msDelay(500);
-//        System.exit(0);
-//    }
-//}
 
